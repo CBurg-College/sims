7451e7c51867b9f9a261e405c5791353240574cd
CBurg-College <cburgmicro@outlook.com>
2026-05-03 15:48:59 +0200

    pxt.json, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,11 +1,11 @@
 /*
-File:      github.com/ETmbit/sims-1.ts
+File:      github.com/CBurg-College/sims.ts
 Copyright: ETmbit, 2026
 
 License:
 This file is part of the ETmbit extensions for MakeCode for micro:bit.
 It is free software and you may distribute it under the terms of the
-GNU General Public License (version 3 or later) as published by the
+GNU etbasic Public License (version 3 or later) as published by the
 Free Software Foundation. The full license text you find at
 https://www.gnu.org/licenses.
 
@@ -13,13 +13,16 @@ Disclaimer:
 ETmbit extensions are distributed without any warranty.
 
 Dependencies:
-ETmbit/general, ETmbit/simonsays
+ETmbit/et-simon
 */
+
+EtSimon.setPins(DigitalPin.P8, DigitalPin.P0, DigitalPin.P1, DigitalPin.P2)
 
 //% color="#66AA22" icon="\uf111"
 //% block="Simon Says"
 //% block.loc.nl="Simon Says"
 namespace SimSays {
+
 
     let ISGAMING = false
 
@@ -27,63 +30,63 @@ namespace SimSays {
     //% block.loc.nl="toon de score"
     export function showPoints() {
         basic.clearScreen()
-        let points = SimonSays.getPoints()
+        let points = EtSimon.getPoints()
         basic.showNumber(points)
-        if (points < 10) General.wait(2)
-        SimonSays.clearColor()
+        if (points < 10) etbasic.wait(2)
+        EtSimon.clearColor()
         basic.showArrow(ArrowNames.West)
     }
 
     //% block="increase the points"
     //% block.loc.nl="verhoog de score"
     export function increasePoints() {
-        SimonSays.increasePoints()
+        EtSimon.increasePoints()
     }
 
     //% block="the wrong color was chosen"
     //% block.loc.nl="de verkeerde kleur werd gekozen"
     export function hasFailed(): boolean {
-        return !SimonSays.isMatchingColor()
+        return !EtSimon.isMatchingColor()
     }
 
     //% block="the right color was chosen"
     //% block.loc.nl="de juiste kleur werd gekozen"
     export function isSuccess(): boolean {
-        return SimonSays.isMatchingColor()
+        return EtSimon.isMatchingColor()
     }
 
     //% block="the chosen color"
     //% block.loc.nl="de gekozen kleur"
-    export function getButtonColor(): Color {
-        return SimonSays.getButtonColor()
+    export function getButtonColor(): ETcolor {
+        return EtSimon.getButtonColor()
     }
 
     //% block="wait until a color is chosen"
     //% block.loc.nl="wacht tot een kleur wordt gekozen"
     export function waitForButton() {
-        SimonSays.waitForButton()
+        EtSimon.waitForButton()
     }
 
     //% block="the displayed color"
     //% block.loc.nl="de getoonde kleur"
-    export function getLedColor(): Color {
-        return SimonSays.getCurrentColor()
+    export function getLedColor(): ETcolor {
+        return EtSimon.getCurrentColor()
     }
 
     //% block="ask the next color"
     //% block.loc.nl="vraag de volgende kleur"
     export function checkNextColor() {
-        SimonSays.setNextColor()
+        EtSimon.setNextColor()
     }
 
     //% block="ask the first color"
     //% block.loc.nl="vraag de eerste kleur"
     export function checkFirstColor() {
-        SimonSays.clearColor()
+        EtSimon.clearColor()
         basic.clearScreen()
-        General.wait(0.5)
+        etbasic.wait(0.5)
         basic.showIcon(IconNames.Heart)
-        SimonSays.setFirstColor()
+        EtSimon.setFirstColor()
     }
 
     //% block="ask all colors"
@@ -91,10 +94,10 @@ namespace SimSays {
     export function checkAllColors() {
         SimSays.checkFirstColor()
         while (SimSays.isInSeries()) {
-            SimonSays.waitForButton()
-            if (SimonSays.isMatchingColor()) {
-                SimonSays.increasePoints()
-                SimonSays.setNextColor()
+            EtSimon.waitForButton()
+            if (EtSimon.isMatchingColor()) {
+                EtSimon.increasePoints()
+                EtSimon.setNextColor()
             }
             else
                 SimSays.stopGame()
@@ -104,23 +107,23 @@ namespace SimSays {
     //% block="show the next color"
     //% block.loc.nl="toon de volgende kleur"
     export function showNextColor() {
-        SimonSays.setNextColor()
-        SimonSays.showCurrentColor()
-        General.wait(0.5)
-        SimonSays.clearColor()
-        General.wait(0.2)
+        EtSimon.setNextColor()
+        EtSimon.showCurrentColor()
+        etbasic.wait(0.5)
+        EtSimon.clearColor()
+        etbasic.wait(0.2)
     }
 
     //% block="show the first color"
     //% block.loc.nl="toon de eerste kleur"
     export function showFirstColor() {
         basic.showIcon(IconNames.SmallHeart)
-        General.wait(0.5)
-        SimonSays.setFirstColor()
-        SimonSays.showCurrentColor()
-        General.wait(0.3)
-        SimonSays.clearColor()
-        General.wait(0.2)
+        etbasic.wait(0.5)
+        EtSimon.setFirstColor()
+        EtSimon.showCurrentColor()
+        etbasic.wait(0.3)
+        EtSimon.clearColor()
+        etbasic.wait(0.2)
     }
 
     //% block="show all colors"
@@ -134,25 +137,25 @@ namespace SimSays {
     //% block="append the series with a color"
     //% block.loc.nl="voeg een kleur aan de serie toe"
     export function appendColor() {
-        SimonSays.extendSeries()
+        EtSimon.extendSeries()
     }
 
     //% block="the next color is required"
     //% block.loc.nl="de volgende kleur nodig is"
     export function isSeriesEnd(): boolean {
-        return (ISGAMING && SimonSays.isSeriesEnd())
+        return (ISGAMING && EtSimon.isSeriesEnd())
     }
 
     //% block="still continuing the series"
     //% block.loc.nl="nog met de serie bezig"
     export function isInSeries(): boolean {
-        return (ISGAMING && SimonSays.isSeriesBusy())
+        return (ISGAMING && EtSimon.isSeriesBusy())
     }
 
     //% block="the next color is required"
     //% block.loc.nl="de volgende kleur nodig is"
     export function restartSeries() {
-        SimonSays.restartSeries()
+        EtSimon.restartSeries()
     }
 
     //% block="the game is busy"
@@ -171,7 +174,7 @@ namespace SimSays {
     //% block="start the game"
     //% block.loc.nl="start het spel"
     export function startGame() {
-        SimonSays.clearSeries()
+        EtSimon.clearSeries()
         ISGAMING = true
     }
 }
